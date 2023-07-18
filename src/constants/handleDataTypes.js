ceccd19882dfed7d12544a467a71db8d007b83e7
thienhs-md <thien.hs@mor.com.vn>
2023-07-18 21:32:42 +0700

    Updates

--- src/constants/handleDataTypes.js
+++
@@ -1,6 +0,0 @@
-export const dataTypes = [
-  { id: 1, value: 'Price: Low to High', sort: 'price_lth' },
-  { id: 2, value: 'Price: High to Low', sort: 'price_htl' },
-  { id: 3, value: 'Rate: Low to High', sort: 'rate_lth' },
-  { id: 4, value: 'Rate: High to Low', sort: 'rate_htl' },
-];
